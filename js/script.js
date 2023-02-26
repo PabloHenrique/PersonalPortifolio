937ae81c600f5badc5f5de037d24cc6828f6d8b8
PabloHenrique <pablohenriquenadai@gmail.com>
2023-02-26 18:39:24 -0300

    2023 02 26 - Skills

--- js/script.js
+++ js/script.js
@@ -25,3 +25,21 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(c => c.addEventListener('click', linkAction))
+
+// ============================= HIDE/OPEN SKILLS =============================
+const skills = document.getElementsByClassName('skills-content'),
+    skillsHeader = document.querySelectorAll('.skills-header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    for(i = 0; i < skills.length; i++){
+        skills[i].className = 'skills-content skills-close'
+    }
+    if(itemClass === 'skills-content skills-close'){
+        this.parentNode.className = 'skills-content skills-open'
+    }
+}
+
+skillsHeader.forEach((e1) => {
+    e1.addEventListener('click', toggleSkills)
+})
