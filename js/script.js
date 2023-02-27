d50ee99dc34d1dd737173b7dacdf4ff7d7a95864
PabloHenrique <pablohenriquenadai@gmail.com>
2023-02-27 10:47:44 -0300

    2023 02 27 - Qualificações

--- js/script.js
+++ js/script.js
@@ -43,3 +43,22 @@ function toggleSkills(){
 skillsHeader.forEach((e1) => {
     e1.addEventListener('click', toggleSkills)
 })
+
+// ============================= QUALIFICAÇÕES =============================
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification-active')
+        })
+        target.classList.add('qualification-active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification-active')
+        })
+        tab.classList.add('qualification-active')
+    })
+})
