cc6295f77e0b066365f653fb445996a3384288ff
PabloHenrique <pablohenriquenadai@gmail.com>
2023-04-02 08:45:30 -0300

    2023 04 02 - Ajustes Gerais + Finalizacao

--- js/script.js
+++ js/script.js
@@ -33,14 +33,17 @@ const tabs = document.querySelectorAll('[data-target]'),
 tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.target)
+
         tabContents.forEach(tabContent =>{
             tabContent.classList.remove('qualification-active')
         })
+
         target.classList.add('qualification-active')
 
         tabs.forEach(tab =>{
             tab.classList.remove('qualification-active')
         })
+        
         tab.classList.add('qualification-active')
     })
 })
